786c1125c8b4021bc751566bf52dc7d12c26ebe7
KitekMaja <maja.kitek@student.um.si>
2023-05-07 14:17:04 +0200

    Changed db location and added initial files for plant.

--- plant-database/src/app.js
+++ plant-database/src/app.js
@@ -1,33 +1,8 @@
 // Import Express
 const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
-
-const { Sequelize } = require('sequelize');
-const { Client } = require('pg');
 
 // Create an instance of Express
 const app = express();
-
-// use .env file where database login data is stored
-dotenv.config({ path: path.resolve(__dirname, 'config/environments', '.env') });
-
-// create DB connection
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
 
 // Define an endpoint
 app.get('/', (req, res) => {
